bfbf10d1e8f70f0d4481018133ff6fcb4ebc2aa8
tonilsonsalvador026-droid <tonilsonsalvador026@gmail.com>
2025-12-18 17:03:38 +0100

    Refactor MainLayout to use <main> tag

--- src/components/layout/MainLayout.js
+++ src/components/layout/MainLayout.js
@@ -5,15 +5,16 @@ import Sidebar from "../ui/Sidebar";
 const MainLayout = ({ children }) => {
   return (
     <div className="flex min-h-screen bg-gray-100">
-      {/* Sidebar */}
+      {/* Sidebar fixa */}
       <Sidebar />
 
       {/* Conteúdo principal */}
-      <div className="flex-1 flex flex-col md:ml-64 p-4 md:p-6 overflow-y-auto transition-all duration-300">
+      <main className="flex-1 ml-64 p-4 md:p-6 overflow-y-auto">
         {children}
-      </div>
+      </main>
     </div>
   );
 };
 
 export default MainLayout;
+
