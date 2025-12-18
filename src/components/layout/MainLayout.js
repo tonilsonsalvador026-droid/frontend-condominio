e902856b299a6bf3751c2474af567f6732e92e00
tonilsonsalvador026-droid <tonilsonsalvador026@gmail.com>
2025-12-18 14:02:25 +0100

    Refactor MainLayout component for layout adjustments

--- src/components/layout/MainLayout.js
+++ src/components/layout/MainLayout.js
@@ -1,28 +1,11 @@
-// src/components/layout/MainLayout.js
-import React from "react";
-import Sidebar from "../ui/Sidebar";
-
 const MainLayout = ({ children }) => {
   return (
     <div className="flex min-h-screen bg-gray-100">
-      {/* Sidebar */}
       <Sidebar />
 
-      {/* Conteúdo principal */}
-      <main
-        className="
-          flex-1
-          w-full
-          p-4
-          sm:p-6
-          lg:ml-64
-          overflow-y-auto
-        "
-      >
+      <main className="ml-56 sm:ml-60 lg:ml-64 w-full p-4 sm:p-6">
         {children}
       </main>
     </div>
   );
 };
-
-export default MainLayout;
