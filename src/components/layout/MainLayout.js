de27220ebf136bfb276e0f27fd2484c77d8b3207
tonilsonsalvador026-droid <tonilsonsalvador026@gmail.com>
2026-02-13 17:02:33 +0100

    Refactor MainLayout to include content wrapper
    Updated the layout structure to include a wrapper div for content.

--- src/components/layout/MainLayout.js
+++ src/components/layout/MainLayout.js
@@ -18,13 +18,12 @@ const MainLayout = () => {
         <Topbar onToggleSidebar={() => setSidebarOpen(!sidebarOpen)} />
 
         {/* Conteúdo */}
-        <main className="flex-1 overflow-y-auto p-4 md:p-6">
-          <Outlet />
-        </main>
-      </div>
-    </div>
+        <main className="flex-1 overflow-y-auto">
+  <div className="w-full max-w-screen-xl mx-auto p-4 md:p-6">
+    <Outlet />
+  </div>
+</main>
   );
 };
 
 export default MainLayout;
-
