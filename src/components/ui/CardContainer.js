efa5f49188465844d0a7f211f856e35d76f2df25
tonilsonsalvador026-droid <tonilsonsalvador026@gmail.com>
2026-02-13 10:15:40 +0100

    Add CardContainer component for UI layout

--- src/components/ui/CardContainer.js
+++ src/components/ui/CardContainer.js
@@ -1,1 +1,11 @@
+import React from "react";
 
+const CardContainer = ({ children }) => {
+  return (
+    <div className="bg-white border border-gray-200 rounded-2xl shadow-sm p-5 md:p-8">
+      {children}
+    </div>
+  );
+};
+
+export default CardContainer;
