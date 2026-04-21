cde482f0167c1707a481118ab7a3fa6e69817f45
tonilsonsalvador026-droid <tonilsonsalvador026@gmail.com>
2026-04-21 10:57:39 +0100

    Refactor App.js to include new payment routes

--- src/App.js
+++ src/App.js
@@ -76,6 +76,8 @@ function App() {
 
           {/* Pagamentos */}
           <Route path="/pagamentos" element={<PagamentoPage />} />
+          <Route path="/pagamentos/novo" element={<PagamentoFormPage />} />
+          <Route path="/pagamentos/:id/editar" element={<PagamentoFormPage />} />
           <Route path="/pagamentos/:id/detalhe" element={<PagamentoDetalhe />} />
           <Route path="/pagamentos/:id/historico" element={<HistoricoPagamento />} />
           <Route path="/pagamentos/eliminados" element={<PagamentoEliminado />} />
