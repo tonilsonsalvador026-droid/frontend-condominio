f8b733c75b1bc715dc53b8493cd240ac7b60065d
tonilsonsalvador026-droid <tonilsonsalvador026@gmail.com>
2026-04-27 16:03:11 +0100

    Implement toggle between ContaCorrenteList and ContaCorrenteForm
    Added functionality to toggle between list and form views.

--- src/components/contacorrente/ContaCorrentePage.js
+++ src/components/contacorrente/ContaCorrentePage.js
@@ -1,8 +1,26 @@
-// src/components/Contacorrente/ContaCorrentePage.js
+// src/components/contacorrente/ContaCorrentePage.js
+import React, { useState } from "react";
 import ContaCorrenteList from "./ContaCorrenteList";
+import ContaCorrenteForm from "./ContaCorrenteForm";
 
 const ContaCorrentePage = () => {
-  return <ContaCorrenteList />;
+  const [showForm, setShowForm] = useState(false);
+
+  return (
+    <div className="w-full">
+
+      {!showForm ? (
+        <ContaCorrenteList
+          onNew={() => setShowForm(true)}
+        />
+      ) : (
+        <ContaCorrenteForm
+          onSuccess={() => setShowForm(false)}
+        />
+      )}
+
+    </div>
+  );
 };
 
 export default ContaCorrentePage;
