0f2236775c0c135658511ab90029c348bb95d0b0
tonilsonsalvador026-droid <tonilsonsalvador026@gmail.com>
2026-04-28 11:20:56 +0100

    Add MovimentoList and enhance ContaCorrentePage logic

--- src/components/contacorrente/ContaCorrentePage.js
+++ src/components/contacorrente/ContaCorrentePage.js
@@ -2,20 +2,31 @@
 import React, { useState } from "react";
 import ContaCorrenteList from "./ContaCorrenteList";
 import ContaCorrenteForm from "./ContaCorrenteForm";
+import MovimentoList from "./MovimentoList";
 
 const ContaCorrentePage = () => {
   const [showForm, setShowForm] = useState(false);
+  const [selectedConta, setSelectedConta] = useState(null);
 
   return (
     <div className="w-full">
 
-      {!showForm ? (
-        <ContaCorrenteList
-          onNew={() => setShowForm(true)}
+      {/* 🔵 1. VER MOVIMENTOS */}
+      {selectedConta ? (
+        <MovimentoList
+          conta={selectedConta}
+          onBack={() => setSelectedConta(null)}
         />
-      ) : (
+      ) : showForm ? (
+        /* 🟢 2. FORMULÁRIO */
         <ContaCorrenteForm
           onSuccess={() => setShowForm(false)}
+        />
+      ) : (
+        /* 🟣 3. LISTA DE CONTAS */
+        <ContaCorrenteList
+          onNew={() => setShowForm(true)}
+          onViewMovimentos={(conta) => setSelectedConta(conta)}
         />
       )}
 
