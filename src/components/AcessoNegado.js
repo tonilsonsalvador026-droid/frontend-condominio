a9a93621dd7afad3aac50518fb482ffffc886ec5
tonilsonsalvador026-droid <tonilsonsalvador026@gmail.com>
2026-06-25 13:20:23 +0100

    Add AcessoNegado component for access denial message

---
+++ src/components/AcessoNegado.js
@@ -0,0 +1,14 @@
+export default function AcessoNegado() {
+return (
+<div className="p-10 text-center">
+<h1 className="text-4xl font-bold text-red-600">
+Acesso Negado
+</h1>
+
+  <p className="mt-4">
+    Não possui permissão para aceder a esta página.
+  </p>
+</div>
+
+);
+}
